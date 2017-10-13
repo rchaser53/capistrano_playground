77c8ff9b09707f2cec5fd9426f3ba2c84b6e3925
rchaser53 <t.yoshizawa@sorabito.com>
2017-10-13 10:58:12 +0900

    try to use "use strict" for suppressing error but failed

--- compare/uglify3.js
+++ compare/uglify3.js
@@ -3,6 +3,9 @@ const path = require('path');
 const UglifyJS = require('uglify-js');
 
 const code = fs.readFileSync(path.resolve(__dirname, './input.js'));
-const result = UglifyJS.minify(code.toString());
+// const result = UglifyJS.minify(code.toString(), {
+
+const result = UglifyJS.minify(`"use strict";
+const aaa = 23;`);
 
 console.log(result)
